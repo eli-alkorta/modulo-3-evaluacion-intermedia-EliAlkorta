867c67c34de8f52da73c8e9d8be020ecea8bcd06
EliAlkorta <elialkor@gmail.com>
2020-04-07 10:44:31 +0200

    publish in github pages

---
+++ docs/precache-manifest.e6146ef1b3a492b89a73c5bdc28f17d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82456b7b1acb4b2360cf5a62fdce1aef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9c1887542e92f1d59e8",
+    "url": "./static/css/main.9143c06b.chunk.css"
+  },
+  {
+    "revision": "2cbfa90505a1def4d50d",
+    "url": "./static/js/2.c4e849cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c4e849cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c1887542e92f1d59e8",
+    "url": "./static/js/main.6ffa439c.chunk.js"
+  },
+  {
+    "revision": "5c7471f8027b20ea2b09",
+    "url": "./static/js/runtime-main.fdecdd34.js"
+  }
+]);
